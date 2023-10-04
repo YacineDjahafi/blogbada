a61e51a44574bb3985172a459d64a5027120bb1d
YacineDjahafi <yacine465@hotmail.fr>
2023-10-04 08:04:30 +0200

    04/10

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+/* const scrollers = document.querySelectorAll(".scroller");
+
+if(!window.matchMedia("(prefers-reduced-motion: reduce").matches){
+    addAnimation();
+}
+
+function addAnimation(){
+    scrollers.forEach(scroller => {
+        scroller.setAttribute('data-animated', true)
+    });
+} */
+
+const miniScroll = document.querySelectorAll(".beforeh");
+
+if (condition) {
+    
+}
